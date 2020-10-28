50b7a7581a09423714293a4ef120a3ff01c7b8c4
yangyxu <945758912@qq.com>
2020-10-28 14:06:15 +0800

    rebuild

--- __/build/Page.js
+++ __/build/Page.js
@@ -8,7 +8,8 @@ module.exports = React.createClass({
   displayName: 'ZRPage',
   getDefaultProps: function getDefaultProps() {
     return {
-      canBack: true
+      canBack: true,
+      loading: false
     };
   },
   componentDidMount: function componentDidMount() {},
@@ -21,6 +22,7 @@ module.exports = React.createClass({
       style: this.props.headerStyle
     }, /*#__PURE__*/React.createElement(PageBar, {
       canBack: this.props.canBack,
+      loading: this.props.loading,
       title: this.props.title,
       leftRender: this.props.leftRender,
       centerRender: this.props.centerRender,
@@ -28,7 +30,23 @@ module.exports = React.createClass({
     })), /*#__PURE__*/React.createElement("div", {
       className: "page-body",
       style: this.props.headerStyle
-    }, this.props.children), /*#__PURE__*/React.createElement("div", {
+    }, this.props.loading ? /*#__PURE__*/React.createElement("div", {
+      className: "loading-container"
+    }, /*#__PURE__*/React.createElement("div", {
+      className: "loader"
+    }, /*#__PURE__*/React.createElement("svg", {
+      "aria-hidden": "true",
+      focusable: "false",
+      "data-prefix": "fas",
+      "data-icon": "sync-alt",
+      className: "self-loading svg-inline--fa fa-sync-alt fa-w-16 ",
+      role: "img",
+      xmlns: "http://www.w3.org/2000/svg",
+      viewBox: "0 0 512 512"
+    }, /*#__PURE__*/React.createElement("path", {
+      fill: "currentColor",
+      d: "M370.72 133.28C339.458 104.008 298.888 87.962 255.848 88c-77.458.068-144.328 53.178-162.791 126.85-1.344 5.363-6.122 9.15-11.651 9.15H24.103c-7.498 0-13.194-6.807-11.807-14.176C33.933 94.924 134.813 8 256 8c66.448 0 126.791 26.136 171.315 68.685L463.03 40.97C478.149 25.851 504 36.559 504 57.941V192c0 13.255-10.745 24-24 24H345.941c-21.382 0-32.09-25.851-16.971-40.971l41.75-41.749zM32 296h134.059c21.382 0 32.09 25.851 16.971 40.971l-41.75 41.75c31.262 29.273 71.835 45.319 114.876 45.28 77.418-.07 144.315-53.144 162.787-126.849 1.344-5.363 6.122-9.15 11.651-9.15h57.304c7.498 0 13.194 6.807 11.807 14.176C478.067 417.076 377.187 504 256 504c-66.448 0-126.791-26.136-171.315-68.685L48.97 471.03C33.851 486.149 8 475.441 8 454.059V320c0-13.255 10.745-24 24-24z"
+    })), /*#__PURE__*/React.createElement("span", null, this.props.loadingText || 'Loading'))) : this.props.children), /*#__PURE__*/React.createElement("div", {
       className: "page-footer",
       style: this.props.footerStyle
     }, this.props.footer));
--- src/PageBar.js
+++ src/PageBar.js
@@ -4,7 +4,8 @@ module.exports = React.createClass({
 	displayName:'ZRPageBar',
 	getDefaultProps:function(){
 		return {
-			canBack: true
+			canBack: true,
+			loading: false
 		}
 	},
 	__renderCenter: function (){
@@ -18,16 +19,22 @@ module.exports = React.createClass({
 	__doBack: function (){
 		window.history.back();
 	},
-	render: function(){
+	__renderIcon: function (){
+		if(this.props.loading) {
+			return <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="circle-notch" className="svg-inline--fa self-loading fa-circle-notch fa-w-16 " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M288 39.056v16.659c0 10.804 7.281 20.159 17.686 23.066C383.204 100.434 440 171.518 440 256c0 101.689-82.295 184-184 184-101.689 0-184-82.295-184-184 0-84.47 56.786-155.564 134.312-177.219C216.719 75.874 224 66.517 224 55.712V39.064c0-15.709-14.834-27.153-30.046-23.234C86.603 43.482 7.394 141.206 8.003 257.332c.72 137.052 111.477 246.956 248.531 246.667C393.255 503.711 504 392.788 504 256c0-115.633-79.14-212.779-186.211-240.236C302.678 11.889 288 23.456 288 39.056z"></path></svg>;
+		}
 		var _canBack = this.props.canBack;
+		if(_canBack) {
+			return <svg onClick={this.__doBack} aria-hidden="true" focusable="false" data-prefix="fas" data-icon="arrow-left" className="svg-inline--fa fa-arrow-left fa-w-14 " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+				<path fill="currentColor" d="M257.5 445.1l-22.2 22.2c-9.4 9.4-24.6 9.4-33.9 0L7 273c-9.4-9.4-9.4-24.6 0-33.9L201.4 44.7c9.4-9.4 24.6-9.4 33.9 0l22.2 22.2c9.5 9.5 9.3 25-.4 34.3L136.6 216H424c13.3 0 24 10.7 24 24v32c0 13.3-10.7 24-24 24H136.6l120.5 114.8c9.8 9.3 10 24.8.4 34.3z"></path>
+			</svg>;
+		}
+	},
+	render: function(){
 		return (
-			<div className={znui.react.classname("zr-page-bar", this.props.className)} style={this.props.style} >
+			<div className={znui.react.classname("zr-page-bar", this.props.className, (this.props.loading?'loading':''))} style={this.props.style} >
 				<div className="bar-left">
-					{
-						_canBack && <svg onClick={this.__doBack} aria-hidden="true" focusable="false" data-prefix="fas" data-icon="arrow-left" className="svg-inline--fa fa-arrow-left fa-w-14 " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-							<path fill="currentColor" d="M257.5 445.1l-22.2 22.2c-9.4 9.4-24.6 9.4-33.9 0L7 273c-9.4-9.4-9.4-24.6 0-33.9L201.4 44.7c9.4-9.4 24.6-9.4 33.9 0l22.2 22.2c9.5 9.5 9.3 25-.4 34.3L136.6 216H424c13.3 0 24 10.7 24 24v32c0 13.3-10.7 24-24 24H136.6l120.5 114.8c9.8 9.3 10 24.8.4 34.3z"></path>
-						</svg>
-					}
+					{this.__renderIcon()}
 					<div className="title">{this.props.title}</div>
 					{ this.props.leftRender && this.props.leftRender(this) }
 				</div>
